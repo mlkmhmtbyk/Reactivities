fce3a3a49441eade6ae5669728707ef748dfd034
Mlkmhmtbyk <biyikm16@itu.edu.tr>
2021-09-03 16:29:22 +0300

    Forgotten variable fix

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -12,7 +12,7 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await Mediator.Send(new List.Query(), ct);
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
